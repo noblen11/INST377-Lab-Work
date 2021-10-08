70f07e5fe16bdf468eda862f2cf20a1f586aa5fe
noblen11 <nwokomanoble5@gmail.com>
2021-10-08 17:18:02 -0400

    so far

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,44 +1,32 @@
 async function windowActions(){
 
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-
-
-const request = await fetch(endpoint)
-.   then(blob => blob.json())
-.   then(data => restraunts.push(...data))
-
-const restraunts= await request.json()
-
-function findmathes(wordToMatch, restraunts){
-    return restraunts.filter(place => {
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  const request = await fetch(endpoint)
+  const restraunts = await request.json()
+  
+  function findMatches(wordToMatch, data) {
+    return data.filter((restraunt) => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      return restruant.name.match(regex) || restraunt.category.match(regex);
+    })
     
-    const regex = new RegExp(wordToMatch, 'gi');
-        return place.restruant.match(regex) || place.type.match(regex) || place.zipCode.match(regex)
-    });
-    
-
-function displayMatches(event){
-    const matchArray = findMatches(this.value, restraunts);
-    const html = matchArray.map(place =>{
-        const regex = new  RegExp(event.target.value, 'gi')
-        const restrauntName = place.restraunt.replace(regex, <span class ="h1">${this.value}</span>);
-        return
-        <li>
-            <span class= "name">{$RestrauntName}</span>
-            <span class= "Type"> ${place.type}</span>
-            <span class ="location"> ${place.zipCode}</span>
-        </li>
-    
-    }) .join('');
-    suggestions.innertHTML = html;
-     
+   function displayMatches(event) {
+      const matchArray = findMatches(this.value, restraunts);
+      const html = matchArray.map((restraunt) => {
+        const regex = new RegExp(event.target.value, 'gi');
+        const restrauntName = restraunt.name.replace(regex, `<span class ="h1">${this.value}</span>`);
+        return `
+            <li>
+                <span class= "name">${restrauntName}</span>
+                <span class= "Type"> ${restruant.category}</span>
+            </li>
+        `;
+      }).join('');
+      suggestions.innertHTML = html;
+    }   
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
+  searchInput.addEventListener('change', displayMatches);
+  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
 }
-
-const searchInput = documant.querySelector('.search')
-const suggestions =document.querySelector('.suggestions');
-
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', (evt) = {displayMatches(evt)})};
-
-window.onload = windowActions;
-}
+ window.onload = windowActions
