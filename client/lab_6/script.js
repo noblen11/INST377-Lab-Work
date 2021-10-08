44cd237f69c792f586ea8df713f5f34e5c50c22a
noblen11 <nwokomanoble5@gmail.com>
2021-10-08 18:06:03 -0400

    new

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,31 +1,34 @@
-async function windowActions(){
+async function windowActions() { 
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-  const request = await fetch(endpoint)
-  const restraunts = await request.json()
+  const request = await fetch(endpoint);
+  const restraunts = await request.json();
   
-  function findMatches(wordToMatch) {
-    return restraunts.filter((restraunt) => {
+  const searchInput = document.querySelector('#restraunt-search');
+  const suggestions = document.querySelector('#suggestions');
+
+  function findMatches(wordToMatch, data) {
+    return data.filter((restraunt) => {
       const regex = new RegExp(wordToMatch, 'gi');
-      return restruant.name.match(regex) || restraunt.category.match(regex);
+      return restraunt.name.match(regex) || restraunt.category.match(regex);
     });
   }
-   function displayMatches(event) {
-      const matchArray = findMatches(this.value, restraunts);
-      const html = matchArray.map((restraunt) => {
-        const regex = new RegExp(event.target.value, 'gi');
-        const restrauntName = restraunt.name.replace(regex, `<span class ="h1">${this.value}</span>`);
-       return `
-            <li>
-                <span class= "name">${restrauntName}</span>
-                <span class= "Type"> ${restruant.category}</span>
-            </li>
-        `;
-      }).join('');
-    suggestions.innertHTML = html;
-  }   
-  const searchInput = document.querySelector('.restraunt-search');
-  const suggestions = document.querySelector('.suggestions');
+
+  function displayMatches(event) {
+    const matchArray = findMatches(this.value, restraunts);
+    const html = matchArray.map((restraunt) => {
+      const regex = new RegExp(event.target.value, 'gi');
+      const restrauntName = restraunt.name.replace(regex, `<span class ="h1">${this.value}</span>`);
+      return `
+          <li>
+              <span class= "name">${restrauntName}</span>
+              <span class= "Type"> ${restraunt.category}</span>
+          </li>
+      `;
+    }).join('');
+    suggestions.innerHTML = html;
+  }
   searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
+  searchInput.addEventListener('keyup', displayMatches);
 }
- window.onload = windowActions
+
+window.onload = windowActions;
